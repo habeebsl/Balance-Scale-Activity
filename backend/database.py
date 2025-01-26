780fc0d2f5edbb48dc96d41b5aaedd7c941424e2
habeeb <habeebomotolani2007@gmail.com>
2025-01-26 09:16:28 +0100

    finishing touches: Romoved unnecessary comments and depreceated code

--- backend/database.py
+++ backend/database.py
@@ -1,7 +1,6 @@
 import os
 from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
-from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker, Session, DeclarativeBase
 
 
 URL_DATABASE =  os.getenv("DATABASE_URL")
@@ -10,4 +9,5 @@ engine = create_engine(URL_DATABASE)
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
-Base = declarative_base()
+class Base(DeclarativeBase):
+   pass
